693cd6dd1a982cfedbce0eb2c1d1b9244d6fe8b7
Ulrich Prinz <ulrich.prinz@googlemail.com>
2014-04-03 16:51:51 +0200

    Added mk.sh and mkimage (u-boot) for confvenience

---
+++ mk.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# create dtb
+make dss-1gb.dtb
+
+# Attach dtb at end of image
+cat arch/arm/boot/zImage arch/arm/boot/dts/dss-1gb.dtb > zImage.tmp
+
+# make uboot image from above result
+mkimage -A arm -O linux -C none -T kernel -a 20008000 -e 20008000 -n linux-3.14 -d zImage.tmp uImage
+rm zImage.tmp
+sudo cp uImage /var/lib/tftpboot/
+
