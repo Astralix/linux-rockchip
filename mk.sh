aefb2ef0290be51a338d691ae73abec359d08f98
Ulrich Prinz <ulrich.prinz@googlemail.com>
2014-07-10 15:45:10 +0200

    Added support for dSS11 SD-card version and reduced size below 2MB.

--- mk.sh
+++ mk.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-INSTALL_MOD_PATH=../dss11_modules
+#
+# Basic path setup
+#
+INSTALL_MOD_PATH=./dss11_modules
 TFTP_PATH=~/tftpboot/
-IMPEXISTS="0"
+
+ARCH=arm
+CROSS_COMPILE=arm-linux-gnueabi-
+
+OUTDIR=../dss11
+
+#
+# Parallel build setup
+#
+NCPU=`grep -c ^processor /proc/cpuinfo`
+let NPRC=$NCPU*2
+
+#
+# build functions
+#
 
 function line {
 	echo "**************************************************"
@@ -24,14 +41,19 @@ function check_ip {
 }
 
 function m_kernel {
+	if [ -z "$1" ]; then
+		echo "Error: give target type [1gb|sdc]"
+		exit 1
+	fi
 	# Compile kernel
-	line "Compiling kernel"
-	make -j9 zImage
+	line "Compiling kernel: $1"
+	make -j$NPRC zImage || exit 1
 	# create dtb
-	line "Compiling DTB"
-	make dss11-1gb.dtb
+	dtsfile="dss11-$1.dtb"
+	line "Compiling DTB: $dtsfile"
+	make $dtsfile || exit 1
 	# Attach dtb at end of image
-	cat arch/arm/boot/zImage arch/arm/boot/dts/dss11-1gb.dtb > zImage.tmp
+	cat arch/arm/boot/zImage arch/arm/boot/dts/$dtsfile > zImage.tmp
 	# make uboot image from above result
 	mkimage -A arm -O linux -C none -T kernel -a 20008000 -e 20008000 -n linux-3.14 -d zImage.tmp uImage
 	rm zImage.tmp
@@ -47,19 +69,14 @@ function i_kernel {
 function m_modules {
 	# Compile Modules
 	line "Compiling modules"
-	make -j9 modules
+	make -j$NPRC modules || exit 1
 	finit
 }
 
 function i_modules {
 	line "Install modules"
 	export INSTALL_MOD_PATH
-	if [ "$IMPEXISTS" = "0" ] ; then
-		export INSTALL_MOD_PATH
-		rm -rf $INSTALL_MOD_PATH
-		mkdir -p $INSTALL_MOD_PATH
-		IMPEXISTS="1"
-	fi
+	mkdir -p $INSTALL_MOD_PATH
 	make modules_install
 	finit
 }
@@ -67,21 +84,26 @@ function i_modules {
 function i_firmware {
 	# Install firmware blobs
 	line "Install all firmware"
-	if [ "$IMPEXISTS" = "0" ] ; then
-		export INSTALL_MOD_PATH
-		rm -rf $INSTALL_MOD_PATH
-		mkdir -p $INSTALL_MOD_PATH
-		IMPEXISTS="1"
-	fi
+	mkdir -p $INSTALL_MOD_PATH
 	make firmware_install
 	finit
 }
 
 function t_modules_firmware {
 	#Install Modules to target
-	line "Installing modules and firmware on target"
+	line "Installing modules and firmware on target at $INSTALL_MOD_PATH"
 	find $INSTALL_MOD_PATH -type l -exec rm {} \;
 	pscp -scp -r $INSTALL_MOD_PATH/ root@$1:/
+	finit
+}
+
+function dl_wifi_drivers {
+	#Download additional WiFi firmware blobs
+	line "Downloading additional WiFi firmware"
+	mkdir -p $INSTALL_MOD_PATH/lib/firmware/rtlwifi
+	pushd $INSTALL_MOD_PATH/lib/firmware/rtlwifi
+	wget -r -l1 --no-parent -A.bin -nd http://ftp2.halpanet.org/source/_dev/linux-firmware.git/rtlwifi/
+	popd
 	finit
 }
 
@@ -91,15 +113,15 @@ while getopts ckmf OPT; do
 			make clean
 			rm -rf $INSTALL_MOD_PATH
 			;;
-		k)	
-			m_kernel
+		k)
+			m_kernel $2
 			i_kernel
 			;;
-		m)	
+		m)
 			m_modules
 			i_modules
 			;;
-		f)	
+		f)
 			i_firmware
 			;;
 		\?)
@@ -109,36 +131,49 @@ while getopts ckmf OPT; do
 	esac
 done
 
+# Remove the switches we parsed above.
 shift `expr $OPTIND - 1`
+
+# Check for left over parameters
 if [ -z $1 ] ; then exit 0; fi
 
 case $1 in
+	sdc)
+		;;
+	1gb)
+		;;
 	setup)
 		line "Setup"
-		export ARCH=arm
-		export CROSS_COMPILE=arm-linux-gnueabi-
+		export ARCH
+		export CROSS_COMPILE
+		export INSTALL_MOD_PATH
 		;;
 	all)
-		check_ip $2
-		m_kernel
+		check_ip $3
+		m_kernel $2
 		i_kernel
 		m_modules
 		i_modules
 		i_firmware
-		t_modules_firmware $2
+		t_modules_firmware $3
 		;;
 	install)	
 		check_ip $2
 		t_modules_firmware $2
 		;;
+	download)
+		dl_wifi_drivers
+		;;
 	*)
-		echo "mk.sh -[c|k|m|f]"
+		echo "mk.sh -[c|k|m|f] [sdc|1gb]"
 		echo "  -c	clean"
 		echo "  -k	build kernel"
 		echo "  -m	build modules"
 		echo "  -f	build firmware"
+		echo "  sdc	build for sd-card model"
+		echo "  1gb	build for 1GB model"
 		echo ""
-		echo "mk.sh [setup|all]"
+		echo "source mk.sh setup"
 		echo "  setup	Call with source to setup toolchain exports."
 		echo "  all	Complete build plus installation."
 		echo ""
@@ -146,5 +181,3 @@ case $1 in
 		echo "  install	Copy over modules and firmware to target."
 		;;
 esac
-
-
